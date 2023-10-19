d2eff321c1299f2769b87912e89815197b94b3b3
gustavo <gustavo.pereira@sou.inteli.edu.br>
2023-10-19 17:48:39 -0300

    tentativa de fix no deploy 3

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,8 +18,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: '2023m8t2-inteli', // Usually your GitHub org/user name.
+  projectName: 'grupo5', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
