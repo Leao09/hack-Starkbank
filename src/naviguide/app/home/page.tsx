df3ddcac3bbcdda77d246f85dab1010989c179f3
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2023-11-10 00:03:50 -0300

    feat: config frontend

---
+++ src/naviguide/app/home/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/navbar';
+
+export default function Home() {
+    return (
+      <>
+        <Navbar />
+        <div>
+          <h1>Bem-vindo à página Home!</h1>
+          {/* Outros conteúdos da sua página Home */}
+        </div>
+      </>
+    );
+  }
