904f2e9b565b59bd13ee23663f814e2e281eb750
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2023-12-07 03:11:35 -0300

    feat: chatbot modal stt

--- src/chatbot_sprint4_sem_chainlit/app.py
+++ src/chatbot_sprint4_sem_chainlit/app.py
@@ -2,9 +2,6 @@ from fastapi import FastAPI, HTTPException, Body, UploadFile, File
 from fastapi.responses import FileResponse
 from gtts import gTTS
 from fastapi.middleware.cors import CORSMiddleware
-from speech_recognition import UnknownValueError, RequestError
-import speech_recognition as sr
-from io import BytesIO
 import os
 
 app = FastAPI()
@@ -26,30 +23,6 @@ async def text_to_speech(text: str = Body(...), lang: str = Body(default="pt-br"
         file_path = "speech.mp3"
         tts.save(file_path)
         return FileResponse(file_path, media_type='audio/mp3', filename=file_path)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-class SpeechToTextNode:
-    def __init__(self):
-        self.recognizer = sr.Recognizer()
-
-    def convert_audio_to_text(self, audio_file):
-        with sr.AudioFile(audio_file) as source:
-            audio_data = self.recognizer.record(source)
-            text = self.recognizer.recognize_google(audio_data, language='pt-br')
-            return text
-
-speech_to_text_node = SpeechToTextNode()
-
-@app.post("/audio-to-text/")
-async def audio_to_text(audio: UploadFile = File(...)):
-    try:
-        temp_file = f"temp_{audio.filename}"
-        with open(temp_file, 'wb') as buffer:
-            buffer.write(await audio.read())
-        text = speech_to_text_node.convert_audio_to_text(temp_file)
-        os.remove(temp_file)
-        return {"text": text}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
--- src/frontend/app/components/chatbotModal.tsx
+++ src/frontend/app/components/chatbotModal.tsx
@@ -1,74 +1,155 @@
 "use client";
-import React, {useState} from 'react';
+import React, { KeyboardEvent, ChangeEvent, useState, useRef, useEffect } from 'react';
+import Link from 'next/link';
 import styles from '../styles/chatbotModal.module.css';
+import axios from 'axios';
 import '../globals.css';
 
-interface ModalProps {
-    onClose: () => void;
+declare global {
+  interface Window {
+    webkitSpeechRecognition: any;
+  }
 }
 
-const ChatbotModal: React.FC<ModalProps> = ({ onClose }) => {
+interface Message {
+  message: string;
+  sender: 'user' | 'bot';
+}
+
+interface ModalProps {
+ onClose: () => void;
+ }
+
+ const ChatbotModal: React.FC<ModalProps> = ({ onClose }) => {
 
   const [inputText, setInputText] = useState('');
+  const [isRecording, setIsRecording] = useState(false);
+  const [messageHistory, setMessageHistory] = useState<Message[]>([]);
+  const recognitionRef = useRef<any>(null);
 
-  const handleInputChange = (event) => {
+  const onCloseButton = () => {
+   console.log("Botão clicado");
+   onClose();
+  };
+
+  const addToMessageHistory = (message: string, sender: 'user' | 'bot') => {
+    setMessageHistory(prevHistory => [...prevHistory, { message, sender }]);
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInputText(event.target.value);
   };
 
-  const handleSendClick = () => {
-    console.log(inputText)
+  const handleSendClick = async () => {
+    if (inputText) {
+      addToMessageHistory(inputText, 'user');
+      try {
+        const response = await axios.post('http://localhost:8000/tts/', { text: inputText });
+        if (response.data) {
+          const audioUrl = URL.createObjectURL(new Blob([response.data]));
+          const audio = new Audio(audioUrl);
+          audio.play();
+          addToMessageHistory('Simulando resposta', 'bot');
+        }
+      } catch (error) {
+        console.error('Erro ao enviar texto para TTS:', error);
+      }
+    }
     setInputText('');
   };
 
-  const handleKeyPress = (event) => {
+  const handleKeyPress = (event: KeyboardEvent) => {
     if (event.key === 'Enter') {
       handleSendClick();
     }
   };
 
-  const onCloseButton = () => {
-    console.log("Botão clicado");
-    onClose();
+  const startRecording = () => {
+    setIsRecording(true);
+    recognitionRef.current = new window.webkitSpeechRecognition();
+    recognitionRef.current.continuous = true;
+    recognitionRef.current.interimResults = true;
+
+    recognitionRef.current.onresult = (event:any) => {
+      const lastResult = event.results[event.results.length - 1];
+      const transcribedText = lastResult[0].transcript;
+      setInputText(transcribedText);
+    };
+
+    recognitionRef.current.start();
   };
 
+  const stopRecording = () => {
+    if (recognitionRef.current) {
+      recognitionRef.current.stop();
+      setIsRecording(false);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (recognitionRef.current) {
+        recognitionRef.current.stop();
+      }
+    };
+  }, []);
+
+  const handleMicrophoneClick = () => {
+    if (!isRecording) {
+      startRecording();
+    } else {
+      stopRecording();
+    }
+  };
 
   return (
     <>
-        <div className={styles.backdrop}> </div>
-        <div className={styles.modal}>
-              <div className={styles.container}>
-              
-                  <button onClick={onCloseButton} type="button" className={styles.backIcon}>
-                    <img src="/arrow-left.png" alt="back"/> 
-                  </button>
+      <div className={styles.backdrop}> </div>
+      <div className={styles.modal}>
+        <div className={styles.container}>
+          
+            <button onClick={onCloseButton} type="button" className={styles.backIcon}>
+              <img src="/arrow-left.png" alt="back"/> 
+            </button>
+          
 
+          <div className={styles.instructionsContainer}>
 
-              <div className={styles.instructionsContainer}>
+            <button type="button" className={styles.volumeIcon}>
+              <img src="/volume-high.svg" alt="volume"/> 
+            </button>
 
-                  <button type="button" className={styles.volumeIcon}>
-                  <img src="/volume-high.svg" alt="volume"/> 
-                  </button>
+            <div className={styles.robotBlueIcon}>
+              <img src='/blue-robot.png' alt='blue_robot'/>
+            </div>
 
-                  <div className={styles.robotBlueIcon}>
-                  <img src='/blue-robot.png' alt='blue_robot'/>
-                  </div>
+            <p className={styles.textoInserido}></p>
 
-                  <p className={styles.textoInserido}></p>
+            <div className={styles.messagesContainer}>
+              {messageHistory.map((msg, index) => (
+                <div
+                  key={index}
+                  className={`${styles.message} ${msg.sender === 'user' ? styles.userMessage : styles.botMessage}`}
+                >
+                  {msg.message}
+                </div>
+              ))}
+            </div>
 
-                  <div className={styles.inputWithIcon}>
-                  <input type="text" placeholder="Digite algo..." className={styles.input} value={inputText} onChange={handleInputChange} onKeyDown={handleKeyPress}/>
-                  <button className={styles.sendIcon} onClick={handleSendClick}>
-                      <img src="/send.png" alt="send"/> 
-                  </button>
+            <div className={styles.inputWithIcon}>
+              <input type="text" placeholder="Digite algo..." className={styles.input} value={inputText} onChange={handleInputChange} onKeyDown={handleKeyPress}/>
+              <button className={styles.sendIcon} onClick={handleSendClick}>
+                  <img src="/send.png" alt="send"/> 
+              </button>
 
-                  <button className={styles.microphoneIcon}>
-                      <img src="/microphone-2.svg" alt="microfone"/> 
-                  </button>
-                  </div>
-                  
-              </div>
+              <button onClick={handleMicrophoneClick} className={styles.microphoneIcon}>
+                  {isRecording ? <img src="/stop.png" alt="parar"/> : <img src="/microphone-2.svg" alt="microfone"/> }
+              </button>
+            </div>
+            
           </div>
         </div>
+      </div>
     </>
   );
 }
